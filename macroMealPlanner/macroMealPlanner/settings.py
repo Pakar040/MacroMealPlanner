d2aeb839db8fec2092f7d5b3a2cf8f5a359fa601
Alek Kariniemi <alekkariniemi1@gmail.com>
2024-07-17 13:28:31 -0700

    Added Static Dir to settings

--- macroMealPlanner/macroMealPlanner/settings.py
+++ macroMealPlanner/macroMealPlanner/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = BASE_DIR / 'templates'
+STATIC_DIR = BASE_DIR / 'static'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'macroMealPlanner.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR],
+        'DIRS': [TEMPLATE_DIR, STATIC_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
