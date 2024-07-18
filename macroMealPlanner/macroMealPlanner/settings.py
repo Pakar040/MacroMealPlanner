be65246f33a5f14cc0141d2ca6ce12fcad89b5dd
Alek Kariniemi <alekkariniemi1@gmail.com>
2024-07-18 11:02:49 -0700

    Handled Static Dir correctly

--- macroMealPlanner/macroMealPlanner/settings.py
+++ macroMealPlanner/macroMealPlanner/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'macroMealPlanner.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR, STATIC_DIR],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
