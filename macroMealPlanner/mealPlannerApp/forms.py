6d78d9c21f5fef877ea9e49e6de9ac5bf1683d58
Alek Kariniemi <alekkariniemi1@gmail.com>
2024-07-25 12:53:46 -0700

    Added Signup, Login, and Logout Functionality

---
+++ macroMealPlanner/mealPlannerApp/forms.py
@@ -0,0 +1,21 @@
+from typing import Any
+from django import forms
+from django.contrib.auth.models import User
+
+class SignupForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+    def save(self, commit: bool = True) -> Any:
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
+    
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
--- macroMealPlanner/mealPlannerApp/views.py
+++ macroMealPlanner/mealPlannerApp/views.py
@@ -1,11 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from .forms import SignupForm, LoginForm
 
 # Create your views here.
 def login_view(request):
-    return render(request, 'mealPlannerApp/login.html')
+    if request.user.is_authenticated:
+        logout(request)
+        return redirect(reverse('mealPlannerApp:login'))
+    elif request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse('mealPlannerApp:macros'))
+            else:
+                messages.error(request, "Invalid Username or Password")
+        else:
+            messages.error(request, "Invalid Username or Password")
+    else:
+        form = LoginForm()
+
+    return render(request, 'mealPlannerApp/login.html', {'form': form})
 
 def signup_view(request):
-    return render(request, 'mealPlannerApp/signup.html')
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(reverse('mealPlannerApp:macros'))
+        else:
+            messages.error(request, form.errors.as_text())
+
+    else:
+        form = SignupForm()
+
+    return render(request, 'mealPlannerApp/signup.html', {'form': form})
 
 def macros_view(request):
     return render(request, 'mealPlannerApp/macros.html')
