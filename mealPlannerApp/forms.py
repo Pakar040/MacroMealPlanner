e6f6f1eacd141fd66ecab64a12bf84f796c83d99
Alek Kariniemi <alekkariniemi1@gmail.com>
2024-08-01 23:30:33 -0700

    Add MacroMealPlanForm
    also removed temporary javascript from plan.html

--- mealPlannerApp/forms.py
+++ mealPlannerApp/forms.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Mapping
 from django import forms
 from django.contrib.auth.models import User
 from .models import Macros, Food
@@ -17,16 +17,36 @@ class SignupForm(forms.ModelForm):
             user.save()
         return user
     
+
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
 
 class MacrosForm(forms.ModelForm):
     class Meta:
         model = Macros
         fields = ['name', 'protein', 'fat', 'carbs']
 
+
 class FoodForm(forms.ModelForm):
     class Meta:
         model = Food
         fields = ['name', 'units', 'amount', 'protein', 'fat', 'carbs']
+
+
+class MacroMealPlanForm(forms.Form):
+
+    foods = forms.MultipleChoiceField(
+        widget=forms.CheckboxSelectMultiple,
+        required=True,
+    )
+
+    macros = forms.ChoiceField(
+        required=True,
+    )
+
+    def __init__(self, *args, **kwargs) -> None:
+        super(MacroMealPlanForm, self).__init__(*args, **kwargs)
+        self.fields['foods'] = [(food.id, food.name) for food in Food.objects.all()]
+        self.fields['macros'] = [(macros.id, macros.name) for macros in Macros.objects.all()]
